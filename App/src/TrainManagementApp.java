776fede25717b6ae5e4b7455d24e679e6520cfb3
Ashwi0612 <vagu.ash0612@gmail.com>
2026-04-01 09:42:35 +0530

    UC2: Add, remove and check bogies using ArrayList

--- App/src/TrainManagementApp.java
+++ App/src/TrainManagementApp.java
@@ -10,5 +10,23 @@ public class TrainManagementApp {
         List<String> trainConsist = new ArrayList<>();
 
         System.out.println("Initial bogie count: " + trainConsist.size());
+
+        // UC2
+
+        trainConsist.add("Sleeper");
+        trainConsist.add("AC Chair");
+        trainConsist.add("First Class");
+
+        System.out.println("\nAfter adding bogies:");
+        System.out.println(trainConsist);
+
+        trainConsist.remove("AC Chair");
+
+        boolean exists = trainConsist.contains("Sleeper");
+        System.out.println("\nDoes Sleeper exist? " + exists);
+
+        System.out.println("\nFinal bogie list:");
+        System.out.println(trainConsist);
+
     }
 }
